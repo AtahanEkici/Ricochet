d2651e7c8f2499df5f3e4b027189c5cfe098c8cc
Atahan Ekici <atahanekici@hotmail.com>
2023-04-03 22:17:42 +0300

    Added some sort of a time control feature
    Fixed some bugs releated to additions.

---
+++ Assets/Scripts/PowerUp/PowerUpManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+public class PowerUpManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        
+    }
+
+    public static void InstantiatePowerUp()
+    {
+
+    }
+}
